b7fca1e5ab19b3097ce1566362f31c8dc2fc671c
Tung Netmaneesuk <nathachanok.n@gmail.com>
2021-04-30 23:51:37 -0700

    Added mimetypes css

--- teamfinder/settings.py
+++ teamfinder/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles' # os.path.join(BASE_DIR, 'staticfiles')
 # Add to test email:
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Add css to mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
