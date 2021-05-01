cfdfc221cf79b6a83698d4dbe8163d00433dcb7d
Tung Netmaneesuk <nathachanok.n@gmail.com>
2021-05-01 06:08:26 -0700

    Add Skills and Applications models to admin.py

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Skill, Profile, Project, Application
 
 # Register your models here.
+admin.site.register(Application)
 admin.site.register(Skill)
 
 class ProjectInline(admin.TabularInline):
@@ -13,3 +14,7 @@ class ApplicationInline(admin.TabularInline):
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     inlines = [ProjectInline, ApplicationInline]
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ApplicationInline]
