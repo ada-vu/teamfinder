77fc2373742c16b3b9b2cfb3edbe40950d7fca84
Tung Netmaneesuk <nathachanok.n@gmail.com>
2021-05-01 05:58:40 -0700

    Add all database models, routes, and basic views

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    
+    path('projects/create/', views.ProjectCreate.as_view(), name="create-projects"),
+    path('projects/', views.ProjectListView.as_view(), name="projects"),
+    path('projects/<int:pk>/', views.ProjectDetailView.as_view(), name="project-detail"),
+    path('projects/applications', views.ProjectApplicationsListView.as_view(), name="project-applications"),
+
+    path('applications/apply/', views.ApplicationCreate.as_view(), name="create-applications"),
+    path('applications/', views.ApplicationListView.as_view(), name="applications"),
+    path('applications/<int:pk>/', views.ApplicationDetailView.as_view(), name="application-detail"),
 ]
