3913e160f7f631d92973861e4c80c3a53623a274
Tung Netmaneesuk <nathachanok.n@gmail.com>
2021-04-30 20:24:06 -0700

    created base_generic.html

--- teamfinder/settings.py
+++ teamfinder/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=o#n--%)1r8rb=nb=ic%xknbwicb!1su6png!65i=vw4r32b9b'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-=o#n--%)1r8rb=nb=ic%xknbwicb!1su6png!65i=vw4r32b9b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
