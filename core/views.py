75a5c27e2fcca5327a876938654a9f08e78b8ef7
Tung Netmaneesuk <nathachanok.n@gmail.com>
2021-05-02 02:13:43 -0700

    Add signup and login backend and fix their top margin

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic.edit import CreateView
 from .models import Skill, Profile, Project, Application
@@ -11,8 +13,10 @@ def index(request):
 def about(request):
     return render(request, 'core/about.html')
 
-def signup(request):
-    return render(request, 'core/signup.html')
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "core/signup.html"
 
 def profile(request):
     return render(request, 'core/user_profile.html')
